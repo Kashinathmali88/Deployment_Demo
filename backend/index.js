7a3590dbe0542067193dca74976ed03335d8f991
kashinath mali <kashinathmali88@gmail.com>
2025-06-29 10:41:09 +0530

    added the origin in env

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://deployment-demo-frontend-rho.vercel.app",
-    ],
+    origin: ["http://localhost:5173", process.env.ORIGIN],
   })
 );
 
